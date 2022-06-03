53a9f1dce40d334316b498b4d41a92f0eb2373d3
mtralka <mtralka@umd.edu>
2022-06-02 23:15:00 -0400

    ⚰️ remove dead code

--- selfPhoto/frontend/app/app/src/pages/IndexPage.vue
+++ selfPhoto/frontend/app/app/src/pages/IndexPage.vue
@@ -10,65 +10,23 @@ const assetStore = useAssetStore();
 const userStore = useUserStore();
 const router = useRouter();
 
-let fakeAssets = [];
 onMounted(async () => {
-  //  assetStore.loadAssets({}).then(() => {
-  //   infiniteScrollContainer.value.resume();
-  // });
-  await userStore.login({
-    username: 'example@example.org',
-    password: 'example',
-  });
+  if (!userStore.getJWT) {
+    await userStore.login({
+      username: 'example@example.org',
+      password: 'example',
+    });
+  }
 
-  await assetStore.refreshAssets();
-  console.log(assetStore.getAssets);
-
-  // for (let i = 0; i < 30; i++) {
-  //   fakeAssets.push({
-  //     id: i,
-  //     thumbnail: `https://random.imagecdn.app/199/3${i
-  //       .toString()
-  //       .padStart(2, '0')}`,
-  //   });
+  // if (assetStore.getAssets.length === 0) {
+  //   await assetStore.loadAssets({ skip: 0 });
   // }
-  // assetStore.assets = fakeAssets;
 });
 
-// const onLoad = (index, done) => {
-//   const numberOfAssets = assetStore.getAssets.length;
-//   const skip = assetStore.calculatePaginationIndex(index);
-
-//   console.log("loading new")
-//   for (let i = index; i < index + 30; i++) {
-//     fakeAssets.push({
-//       id: i,
-//       thumbnail: `https://random.imagecdn.app/199/3${i
-//         .toString()
-//         .padStart(2, '0')}`,
-//     });
-//   }
-//   done()
-//   assetStore.assets = fakeAssets;
-//   // assetStore
-//   //   .loadAssets({
-//   //     skip: skip,
-//   //   })
-//   //   .then(() => {
-//   //     if (assetStore.getAssets.length === numberOfAssets) {
-//   //       done(true);
-//   //     } else {
-//   //       done();
-//   //     }
-//   //   });
-// };
-
-// const navigateToAsset = (asset, index) => {
-//   assetStore.currentAsset = asset;
-//   assetStore.currentAssetIndex = index;
-
-//   // TODO
-//   router.push({ path: `/m/${asset.id}` });
-// };
+// on refresh
+// assetStore.refreshAssets().then(() => {
+//     done();
+//   });
 </script>
 
 <template>
